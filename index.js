42fa1f2bc41ce836b6f29729890a6ffefa2b6381
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-08 11:32:19 -0500

    figured out how to add the hours with the hours, minutes with minutes, etc

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function stat(strg) {
   // your code
-  console.log(strg);
+  //do i need to seperate the hour minute and seconds to get the average median and range?
+  let array1 = strg.split(", ");
+  console.log(array1);
+  let array2 = [];
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+  let object = {};
+  //I think I want to organize all of the hours, minutes, and seconds in one place
+  //creating a nested array
+  for (let i = 0; i < array1.length; i++) {
+    array2.push(array1[i].split("|"));
+  }
+  //i can make an array of objects
+  //[{hour: 0, minute, seconds: 0}]
+
+  for (let i = 0; i < array2.length; i++) {
+    hours += parseInt(array2[i][0], 10);
+    minutes += parseInt(array2[i][1], 10);
+    seconds += parseInt(array2[i][2], 10);
+  }
+
+  console.log(hours + " hours");
+  console.log(minutes + " minutes");
+  console.log(seconds + " seconds");
 }
 
 stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17");
