9cc6a2840707ebb83b35c3ca2b1d82f690f6f4a0
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-08 12:24:48 -0500

    found the Average and how to write that, attemptedto find the median but I have to figure out how to sort everything and i think I may actually have to make that object

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function stat(strg) {
   // your code
   //do i need to seperate the hour minute and seconds to get the average median and range?
   let array1 = strg.split(", ");
-  console.log(array1);
   let array2 = [];
   let hours = 0;
   let minutes = 0;
@@ -15,7 +14,7 @@ function stat(strg) {
   }
   //i can make an array of objects
   //[{hour: 0, minute, seconds: 0}]
-
+  //to get the Mean
   for (let i = 0; i < array2.length; i++) {
     hours += parseInt(array2[i][0], 10);
     minutes += parseInt(array2[i][1], 10);
@@ -25,9 +24,27 @@ function stat(strg) {
   let totalInSeconds = hours * 3600 + minutes * 60 + seconds;
   let averageInSeconds = parseInt(totalInSeconds / array1.length, 10);
   let meanSeconds = (averageInSeconds % 3600) % 60;
+  let meanMinutesBefore = (averageInSeconds - meanSeconds) / 60;
+  let meanMinutes = meanMinutesBefore % 60;
+
+  let meanHours = (meanMinutesBefore - meanMinutes) / 60;
+  console.log(meanHours);
+
+  console.log(meanMinutes);
+  //   let numberOf3600s = parseInt((averageInSeconds - meanSeconds) / 3600, 10);
+  //   let meanMinutes =
   console.log(meanSeconds);
-  console.log(totalInSeconds);
-  console.log(averageInSeconds);
+
+  let average = "Average: " + meanHours + "|" + meanMinutes + "|" + meanSeconds;
+  console.log(average);
+
+  //didn't work because I have to sort everything first
+  let median = 0;
+  if (array1.length % 2 !== 0) {
+    median = array1[Math.round(array1.length / 2)];
+  }
+
+  console.log(median);
 }
 
 stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17");
